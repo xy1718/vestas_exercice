ed23ccc3436b063c757ff7f11e376b669cf2b9b7
Luis Anjos <xy1718@hotmail.com>
2019-02-27 10:13:18 +0000

    Add files via upload

---
+++ Vestas_DevAssessement2/Vestas_DevAssessement2/Product.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vestas_DevAssessement2
+
+{
+
+    // Simple business object with the information that retrieve the list of product available for sale with (prices, stocks, ...)
+
+    public class Product : IEquatable<Product>
+    {
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; }
+
+        public double Price { get; set; }
+
+        public int StockPromo_Buy { get; set; }
+
+        public int StockPromo_Pay { get; set; }
+
+        public int LastQuantityPurchased { get; set; }
+
+        public override string ToString()
+        {
+            return "ProductName: " + ProductName + "   Price: " + Price.ToString() + "   Buy: " + StockPromo_Buy.ToString() + "   Pay: " + StockPromo_Pay.ToString() + "   Purchased: " + LastQuantityPurchased.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            return ProductID;
+        }
+
+        public bool Equals(Product other)
+        {
+            if (other == null) return false;
+            return (this.ProductName.Equals(other.ProductName));
+        }
+
+        /// <summary>
+        ///     Creation of the test list of products with the required information of prices and stock promotion/discount (type buy x -> pay y)
+        /// </summary>
+        /// <returns> Retrieve a Product list with information about stock and prices of the items according the example </returns>
+        public List<Product> ProductStocksList()
+        {
+
+            // Add product items for test to the list.
+            List<Product> ProductsList = new List<Product>();
+
+            //The basket can contain any item multiple times. Items are priced as follows:
+
+            //- Apples are 0.25€ each
+            ProductsList.Add(new Product() { ProductID = 0, ProductName = "Apple", Price = 0.25, StockPromo_Buy = 1, StockPromo_Pay = 1, LastQuantityPurchased = 0 });
+
+            //- Oranges are 0.30€ each
+            ProductsList.Add(new Product() { ProductID = 1, ProductName = "Orange", Price = 0.3, StockPromo_Buy = 1, StockPromo_Pay = 1, LastQuantityPurchased = 0 });
+
+            //- Bananas are 0.15€ each
+            ProductsList.Add(new Product() { ProductID = 2, ProductName = "Banana", Price = 0.15, StockPromo_Buy = 1, StockPromo_Pay = 1, LastQuantityPurchased = 0 });
+
+            //- Papayas are €0.50 each, but are available as "get three, pay two"
+            ProductsList.Add(new Product() { ProductID = 3, ProductName = "Papaya", Price = 0.5, StockPromo_Buy = 3, StockPromo_Pay = 2, LastQuantityPurchased = 0 });
+
+            return ProductsList;
+        }
+
+    }
+}
---
+++ Vestas_DevAssessement2/Vestas_DevAssessement2/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vestas_DevAssessement2
+{
+    //**+ luis.anjos@outlook.com
+    // Coding exercise 
+    // Write a simple program that calculates the price of a shopping basket.
+
+    class CodingExercise
+    {
+        static void Main(string[] args)
+        {
+
+            ///The output should be displayed similar to what you would expect to see on a receipt.
+            //Example input:
+            //Apple
+            //Apple
+            //Orange
+            //Apple
+            //Papaya
+            //Banana
+            //Papaya
+            //Papaya
+            //Example Output:
+            //Total: 2.2€
+
+            #region INPUT - Data entry and basic validation
+
+            // Local instance of the product class in order to handle prices and discount information      
+            Product objProduct = new Product();
+
+            // UI welcome splash      
+            Console.WriteLine("List of products to sell:");
+            Console.WriteLine();
+
+            // Products available to sell 
+            List<Product> lstProductsToSell = objProduct.ProductStocksList();
+
+            foreach (Product item in lstProductsToSell)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            // Given a list of shopping, calculate the total cost of those items.
+            List<Product> lstShoppingList = new List<Product>();
+
+            // UI data entry
+            Console.WriteLine();
+            Console.WriteLine("Input the shopping list (product by product):");
+            Console.WriteLine("-- To calculate the total costs, leave the empty string and do ENTER again --");
+            Console.WriteLine();
+
+            // Items are presented one at a time, each of it in one line, as a list, identified by their name (Apples,Bananas).
+            //The basket can contain any item multiple times.
+
+            string ProductItemInput;
+            do
+            {
+                ProductItemInput = Console.ReadLine();
+
+                // Fill the shopping list with valid products
+                if (lstProductsToSell.Exists(x => x.ProductName.Contains(ProductItemInput)))
+                {
+                    // Get the index/ID of the product to simplify the calculations 
+                    int index = lstProductsToSell.FindIndex(x => x.ProductName == ProductItemInput);
+                    // Add valid product in the shopping list
+                    lstShoppingList.Add(new Product() { ProductName = ProductItemInput, ProductID = index });
+
+                }
+                else
+                    Console.WriteLine("Please input a valid product name!!!!!");
+
+            } while (ProductItemInput.ToString() != String.Empty);
+
+            #endregion
+
+            #region OUTPUT - Total cost calculation 
+
+            double totalCost = 0;
+
+            // Processing shopping list; counting items purchased
+            foreach (Product item in lstShoppingList)
+            {
+                // to avoid UI data entry weakness 
+                if (item.ProductName != String.Empty)
+                    // Updating the property with the last quantity purchased to calc the itens of the current basket 
+                    lstProductsToSell[item.ProductID].LastQuantityPurchased = lstProductsToSell[item.ProductID].LastQuantityPurchased + 1;
+
+            }
+
+            // Calc totals costs
+
+            foreach (Product item in lstProductsToSell)
+            {
+                // If exist any promo discount related with the quantities purchased
+                if (item.StockPromo_Buy > 1)
+                {
+                    //  Calc the discount quantity 
+                    int quantityDiscountPromo = item.StockPromo_Buy - item.StockPromo_Pay;
+
+                    // If the item is able to apply the promo/discount
+                    if (item.LastQuantityPurchased >= item.StockPromo_Buy)
+                        //TODO: is need a round function to avoid run time errors
+                        totalCost = totalCost + (item.Price * (item.LastQuantityPurchased - (item.LastQuantityPurchased / item.StockPromo_Buy) * quantityDiscountPromo));
+                    else
+                        totalCost = totalCost + (item.Price * item.LastQuantityPurchased);
+                }
+                else
+                    totalCost = totalCost + (item.Price * item.LastQuantityPurchased);
+
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.WriteLine("Total cost of this shopping basket:");
+            Console.WriteLine(totalCost.ToString());
+            Console.ReadKey();
+
+            #endregion
+
+        }
+
+    }
+}
